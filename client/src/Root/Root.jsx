a8644cd17411ae621ac6852721a02b2a423b45c8
Ashish Sarker <your_github_email@example.com>
2026-02-11 18:58:36 +0600

    footer added namely but not much modified

--- client/src/Root/Root.jsx
+++ client/src/Root/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
 
 const Root = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
