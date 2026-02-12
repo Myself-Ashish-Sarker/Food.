d2e84b621755ef05ed3d3530c42db6930edee11e
Ashish Sarker <your_github_email@example.com>
2026-02-12 18:54:47 +0600

    mongodb connection established

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@mongo-app.q62fegx.mongodb.net/?appName=mongo-app`;
 const port = process.env.PORT || 5000;
 
 // middlewars
@@ -10,7 +12,36 @@ app.use(express.json());
 
 
 
+// mongodb area
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+// mongodb area
 
 
 app.get("/", (req, res) => {
